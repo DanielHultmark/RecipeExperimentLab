5c6ab07ad4b044148c7bb3c05cfce8d60d169084
Daniel Hultmark <danielhultmark@gmail.com>
2026-09-02 11:07:57 +0200

    Add project files.

---
+++ Data/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeExperimentLab.Data
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Data/Score.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeExperimentLab.Data
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public int NumberScore { get; set; }
+    }
+}
